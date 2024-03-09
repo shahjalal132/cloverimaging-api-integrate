0ecb6b87a2a6357407478433ef87b7e40553aa01
Shah jalal <rjshahjalal132@gmail.com>
2024-03-09 12:13:23 +0600

    initial commit

---
+++ cloverimaging-api.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * Plugin Name:       cloverimaging api integrate
+ * Plugin URI:        #
+ * Description:       cloverimaging api integrate
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            imjol
+ * Author URI:        https://imjol.com/
+ */
+
+ 
