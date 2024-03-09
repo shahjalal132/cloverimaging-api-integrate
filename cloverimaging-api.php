132faa0f3ad91e0a3a9c7510d57c3bad44bfb6f7
Shah jalal <rjshahjalal132@gmail.com>
2024-03-09 12:17:01 +0600

    create some files

--- cloverimaging-api.php
+++ cloverimaging-api.php
@@ -11,4 +11,15 @@
  * Author URI:        https://imjol.com/
  */
 
- 
+defined( "ABSPATH" ) || exit( "Direct Access Not Allowed" );
+
+// Define plugin path
+if ( !defined( 'CLOVER_PLUGIN_PATH' ) ) {
+    define( 'CLOVER_PLUGIN_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
+}
+
+// Define plugin url
+if ( !defined( 'CLOVER_PLUGIN_URI' ) ) {
+    define( 'CLOVER_PLUGIN_URI', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
+}
+
