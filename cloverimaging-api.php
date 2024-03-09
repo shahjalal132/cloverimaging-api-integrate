1043561759bbf6cd2a34ef73d667e1264f9c361a
Shah jalal <rjshahjalal132@gmail.com>
2024-03-09 12:41:43 +0600

    create database tables when plugin acrivated

--- cloverimaging-api.php
+++ cloverimaging-api.php
@@ -23,3 +23,25 @@ if ( !defined( 'CLOVER_PLUGIN_URI' ) ) {
     define( 'CLOVER_PLUGIN_URI', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
 }
 
+
+//Create sync_clover_product_types table when the plugin activate
+register_activation_hook( __FILE__, 'clover_product_types_db_table_create' );
+
+//Create sync_clover_products table when the plugin activate
+register_activation_hook( __FILE__, 'clover_products_db_table_create' );
+
+//Create wp_sync_clover_availability table when the plugin activate
+register_activation_hook( __FILE__, 'clover_availability_db_table_create' );
+
+//Create wp_sync_clover_prices table when the plugin activate
+register_activation_hook( __FILE__, 'clover_prices_db_table_create' );
+
+//Create wp_sync_clover_invoices table when the plugin activate
+register_activation_hook( __FILE__, 'clover_invoices_db_table_create' );
+
+
+
+// require files
+require_once CLOVER_PLUGIN_PATH . '/inc/clover-create-db-tables.php';
+require_once CLOVER_PLUGIN_PATH . '/inc/clover-helper-functions.php';
+require_once CLOVER_PLUGIN_PATH . '/inc/clover-fetch-api-insert-to-db.php';
--- inc/clover-create-db-tables.php
+++ inc/clover-create-db-tables.php
@@ -1,2 +1,123 @@
 <?php
 
+/**
+ * Create DB Tables
+ * 
+ * Get-all-product-types
+ * products
+ * availability
+ * prices
+ * invoices
+ */
+
+
+// Create sync_clover_product_types Table When Plugin Activated
+function clover_product_types_db_table_create() {
+
+    global $wpdb;
+
+    $table_name      = $wpdb->prefix . 'sync_clover_product_types';
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE IF NOT EXISTS $table_name (
+        id INT AUTO_INCREMENT,
+        operation_type VARCHAR(255) NOT NULL,
+        operation_value TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+        PRIMARY KEY (id)
+    ) $charset_collate;";
+
+    require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+    dbDelta( $sql );
+}
+
+// Create wp_sync_clover_products Table When Plugin Activated
+function clover_products_db_table_create() {
+
+    global $wpdb;
+
+    $table_name      = $wpdb->prefix . 'sync_clover_products';
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE IF NOT EXISTS $table_name (
+        id INT AUTO_INCREMENT,
+        operation_type VARCHAR(255) NOT NULL,
+        operation_value TEXT NOT NULL,
+        status VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+        PRIMARY KEY (id)
+    ) $charset_collate;";
+
+    require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+    dbDelta( $sql );
+}
+
+// Create wp_sync_clover_availability Table When Plugin Activated
+function clover_availability_db_table_create() {
+
+    global $wpdb;
+
+    $table_name      = $wpdb->prefix . 'wp_sync_clover_availability';
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE IF NOT EXISTS $table_name (
+        id INT AUTO_INCREMENT,
+        operation_type VARCHAR(255) NOT NULL,
+        operation_value TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+        PRIMARY KEY (id)
+    ) $charset_collate;";
+
+    require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+    dbDelta( $sql );
+}
+
+// Create wp_sync_clover_prices Table When Plugin Activated
+function clover_prices_db_table_create() {
+
+    global $wpdb;
+
+    $table_name      = $wpdb->prefix . 'wp_sync_clover_prices';
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE IF NOT EXISTS $table_name (
+        id INT AUTO_INCREMENT,
+        operation_type VARCHAR(255) NOT NULL,
+        operation_value TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+        PRIMARY KEY (id)
+    ) $charset_collate;";
+
+    require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+    dbDelta( $sql );
+}
+
+// Create wp_sync_clover_invoices Table When Plugin Activated
+function clover_invoices_db_table_create() {
+
+    global $wpdb;
+
+    $table_name      = $wpdb->prefix . 'wp_sync_clover_invoices';
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE IF NOT EXISTS $table_name (
+        id INT AUTO_INCREMENT,
+        operation_type VARCHAR(255) NOT NULL,
+        operation_value TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+        PRIMARY KEY (id)
+    ) $charset_collate;";
+
+    require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+    dbDelta( $sql );
+}
