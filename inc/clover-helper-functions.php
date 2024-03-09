0ff4394841a10ebce5e895333464035afa2bfb13
Shah jalal <rjshahjalal132@gmail.com>
2024-03-09 13:13:23 +0600

    insert product types into databse successfully

--- inc/clover-helper-functions.php
+++ inc/clover-helper-functions.php
@@ -1,2 +1,25 @@
 <?php
 
+/**
+ * Function to save response data to a file.
+ *
+ * @param string $data The response data to be saved.
+ */
+function put_product_types_response_data( $data ) {
+    // Ensure directory exists to store response data
+    $directory = CLOVER_PLUGIN_PATH . '/data/';
+    
+    if ( !file_exists( $directory ) ) {
+        mkdir( $directory, 0777, true );
+    }
+
+    // Construct file path for response data
+    $fileName = $directory . 'product_types.json';
+
+    // Write response data to file
+    if ( file_put_contents( $fileName, $data ) !== false ) {
+        return "Data written to file successfully.";
+    } else {
+        return "Failed to write data to file.";
+    }
+}
