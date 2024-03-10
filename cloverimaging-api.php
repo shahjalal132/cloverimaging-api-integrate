d2ff239a85159f3cb27c40e28315d228c3f285d5
Shah jalal <rjshahjalal132@gmail.com>
2024-03-10 12:45:23 +0600

    modified autoload file require position

--- cloverimaging-api.php
+++ cloverimaging-api.php
@@ -23,6 +23,9 @@ if ( !defined( 'CLOVER_PLUGIN_URI' ) ) {
     define( 'CLOVER_PLUGIN_URI', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
 }
 
+// require autoload file
+require_once CLOVER_PLUGIN_PATH . '/vendor/autoload.php';
+
 
 //Create sync_clover_product_types table when the plugin activate
 register_activation_hook( __FILE__, 'clover_product_types_db_table_create' );
@@ -38,7 +41,3 @@ register_activation_hook( __FILE__, 'clover_prices_db_table_create' );
 
 //Create wp_sync_clover_invoices table when the plugin activate
 register_activation_hook( __FILE__, 'clover_invoices_db_table_create' );
-
-
-// require autoload file
-require_once CLOVER_PLUGIN_PATH . '/vendor/autoload.php';
